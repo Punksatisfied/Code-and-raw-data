0fd2c8a3ec505688bb728ab91454ac3788e2da51
Punksatisfied <127203250+Punksatisfied@users.noreply.github.com>
2025-07-23 15:32:41 +0800

    Delete Code/polygonArea.m

--- Code/polygonArea.m
+++
@@ -1,6 +0,0 @@
-function area = polygonArea(points)
-    % points: N×2 的点集合 (x, y)
-    x = points(:, 1);
-    y = points(:, 2);
-    area = 0.5 * abs(sum(x .* circshift(y, -1)) - sum(y .* circshift(x, -1)));
-end
